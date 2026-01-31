2b838acad080556bd06daad05915f8a9ea9f263d
codingguysuk <john@the-coding-guys.co.uk>
2026-01-31 21:55:58 +0000

    Update class-rest-controller.php
    Add a GET endpoint for listing

--- includes/class-rest-controller.php
+++ includes/class-rest-controller.php
@@ -36,6 +36,12 @@ class TCG_SAN_Rest_Controller {
             'callback' => [$this, 'delete'],
             'permission_callback' => [$this, 'permissions'],
         ]);
+
+        register_rest_route('tcg/v1', '/notes', [
+            'methods'  => 'GET',
+            'callback' => [$this, 'index'],
+            'permission_callback' => [$this, 'permissions'],
+        ]);
     }
 
     /**
@@ -62,5 +68,17 @@ class TCG_SAN_Rest_Controller {
     public function delete($request) {
         $this->repo->delete((int) $request['id']);
         return rest_ensure_response(['success' => true]);
+    }
+    public function index($request) {
+        $page   = max(1, (int) $request->get_param('page'));
+        $search = sanitize_text_field($request->get_param('search'));
+
+        $per_page = 5;
+        $offset   = ($page - 1) * $per_page;
+        
+        return rest_ensure_response([
+            'data'  => $this->repo->paginate($per_page, $offset, $search),
+            'total' => $this->repo->count(),
+        ]);
     }
 }
